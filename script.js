b63ef8311a856cd56bc703e64a34f7b56ae1758c
Manuel Rascon <mrascon19@cnm.edu>
2023-07-03 23:45:25 -0600

    added event listiner for projectile

--- script.js
+++ script.js
@@ -30,11 +30,37 @@ class Player {
     }
 }
 
+//This will create a projectile being shot to the middle of the screen
+class Projectile {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+    draw () {
+        //Specifies we want to draw on the screen
+        context.beginPath()
+        //Arc function: context.arc(x: Int, y: Int, r: Int, startAngle: Float, endAngle: Float, drawCounterClockwise: Boolean)
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        //Specifies players color: Blue
+        context.fillStyle = this.color
+        context.fill()
+    }
+}
+
 //This will set the x coordinate to the middle of the screen by dividing the width by half
 const x = canvas.width / 2
+//This will set the y coordinate to the middle of the screen by dividing the height by half
+const y = canvas.height / 2
 
-const player = new Player(100, 100, 30, 'purple')
+const player = new Player(x, y, 30, 'purple')
 //This will tell the player to call the draw() function
 player.draw()
 
 console.log(player)
+
+addEventListener('click', (event) => {
+    const Projectile = new Projectile()
+})
