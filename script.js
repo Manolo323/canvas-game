347611088c56f9b542606457ea320b1f07198cc5
Manuel Rascon <mrascon19@cnm.edu>
2023-07-09 21:34:04 -0600

    added enemies

--- script.js
+++ script.js
@@ -56,6 +56,32 @@ class Projectile {
         this.y = this.y + this.velocity.y
     }
 }
+//This will create an enemy
+class Enemy {
+    constructor(x, y, radius, color, velocity) {
+        this.x = x
+        this.y = y
+        this.radius = radius
+        this.color = color
+        this.velocity = velocity
+    }
+    draw () {
+        //Specifies we want to draw on the screen
+        context.beginPath()
+        //Arc function: context.arc(x: Int, y: Int, r: Int, startAngle: Float, endAngle: Float, drawCounterClockwise: Boolean)
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        //Specifies players color: Blue
+        context.fillStyle = this.color
+        context.fill()
+    }
+
+    update() {
+        this.draw()
+        //This will move projectile from center by adding velocity to x and y coordinates
+        this.x = this.x + this.velocity.x
+        this.y = this.y + this.velocity.y
+    }
+}
 
 //This will set the x coordinate to the middle of the screen by dividing the width by half
 const x = canvas.width / 2
@@ -63,31 +89,59 @@ const x = canvas.width / 2
 const y = canvas.height / 2
 
 const player = new Player(x, y, 30, 'purple')
-//This will tell the player to call the draw() function
-player.draw()
-
 //Renders multiple particles within animate loop by creating an array
 const projectiles =[]
+//Contains each instance for each enemy we create
+const enemies = []
 
+//This will spawn enemies
+function spawnEnemies() {
+    setInterval(() => {
+        const x = 100
+        const y = 100
+        const radius = 30
+        const color = 'blue'
+        const velocity = {
+            x: 1,
+            y:  1
+        }
+
+        enemies.push(new Enemy(x, y, radius, color, velocity))
+
+        console.log(enemies)
+    }, 1000)
+}
 //Animation loop will allow projectile to move from center to where ever we click on the screen
 function animate() {
     requestAnimationFrame(animate)
+    context.clearRect(0 ,0 , canvas.width, canvas.height)
+    //This will tell the player to call the draw() function
+    player.draw()
     projectiles.forEach(projectile => {
         projectile.update()
-        }
-    )
+        })
+
+    enemies.forEach(enemy => {
+        enemy.update()
+    })
 }
 
 //Activates code when a user clicks on the window by listening to click event
 addEventListener('click', (event) => {
     //This will set the velocity of the projectiles using an equation to set the angle
-    const angle = Math.atan2(event.clientY - canvas.height / 2, event.clientX - canvas.width / 2)
+    const angle = Math.atan2(
+        event.clientY - canvas.height / 2,
+        event.clientX - canvas.width / 2
+    )
+    //Sets velocity
+    const velocity = {
+        x: Math.cos(angle),
+        y: Math.sin(angle)
+    }
     projectiles.push(new Projectile(
-        canvas.width / 2, canvas.height / 2, 5, 'red', {
-            x: 1,
-            y: 1
-        })
+        canvas.width / 2, canvas.height / 2, 5, 'red', velocity)
     )
 })
 
 animate()
+spawnEnemies()
