29f2d527cdd515553b022570aa04263bbafec829
Manuel Rascon <mrascon19@cnm.edu>
2023-07-09 22:12:54 -0600

    detected collision on enemy when projectile hits

--- script.js
+++ script.js
@@ -135,8 +135,20 @@ function animate() {
         projectile.update()
         })
 
-    enemies.forEach(enemy => {
+    enemies.forEach((enemy, index) => {
         enemy.update()
+
+        projectiles.forEach((projectile, projectileIndex) => {
+            // Gets distance between projectile and enemy
+            const dist = Math.hypot(projectile.x - enemy.x, projectile.y - enemy.y)
+            //removes enemy once projectile hits enemy
+            if (dist -  enemy.radius - projectile.radius < 1) {
+                setTimeout(()  => {
+                enemies.splice(index, 1)
+                projectiles.splice(projectileIndex, 1)
+                }, 0)
+            }
+        })
     })
 }
 
